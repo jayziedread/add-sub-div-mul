6be9888aa1afd5720da4ab0149909dedb0d97650
jayziedread <gm8614@gmail.com>
2023-05-27 12:58:06 +0800

    simple program that shows add-sub-div-mul in javaScript

---
+++ index.js
@@ -0,0 +1,40 @@
+let num1 = 8
+let num2 = 2
+
+
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el");
+let sum = 0;
+
+function add(){
+
+sum = num1 + num2;
+sumEl.innerText = "Sum: " + sum;
+
+};
+
+function subtract(){
+
+sum = num1 - num2;
+sumEl.innerText = "Sum: " + sum;
+
+
+};
+
+function divide(){
+
+sum = num1 / num2;
+sumEl.innerText = "Sum: " + sum;
+
+
+};
+
+function multiply(){
+
+sum = num1 * num2;
+sumEl.innerText = "Sum: " + sum;
+
+
+};
